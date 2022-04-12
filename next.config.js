ca663653f4b986d2e825a9194bbc27356936d08d
Ibrahim El Gohary <goharyibrahim@gmail.com>
2022-04-12 23:07:21 +0200

    nextauthurl environment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 
-module.exports = nextConfig
+module.exports = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      env: {
+        APP_URL: "http://localhost:3000",
+        NEXTAUTH_URL: "http://localhost:3000",
+      },
+    };
+  }
+
+  return {
+    env: {
+      APP_URL: "https://inventory-manager-nextjs-v4.vercel.app",
+      NEXTAUTH_URL: "https://inventory-manager-nextjs-v4.vercel.app/",
+    },
+  };
+};
